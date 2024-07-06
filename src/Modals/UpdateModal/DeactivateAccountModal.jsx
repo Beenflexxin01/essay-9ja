44e301546061d9da1fd5ebd37a0fe344a62cb350
Adenola Omotayo <adenolaomotayo@gmail.com>
2024-07-06 11:10:22 +0100

    Fixed an Error

--- src/Modals/UpdateModal/DeactivateAccountModal.jsx
+++ src/Modals/UpdateModal/DeactivateAccountModal.jsx
@@ -1,6 +1,6 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import btn from "../../../public/images/delete.png";
+import btn from "../../../public/images/icon.png";
 
 function DeactivateAccountModal(props) {
   return (
