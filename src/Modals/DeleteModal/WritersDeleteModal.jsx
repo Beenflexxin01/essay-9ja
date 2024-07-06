6ede3105d8f9e0df013f5f1a708d17287763dae8
Adenola Omotayo <adenolaomotayo@gmail.com>
2024-07-06 10:15:19 +0100

    Fixed an error as regarding an image

--- src/Modals/DeleteModal/WritersDeleteModal.jsx
+++ src/Modals/DeleteModal/WritersDeleteModal.jsx
@@ -1,6 +1,6 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import btn from "/images/delete.png";
+import btn from "../../../public/images/delete.png";
 function WritersDeleteModal(props) {
   return (
     <>
--- src/Modals/UpdateModal/DeactivateAccountModal.jsx
+++ src/Modals/UpdateModal/DeactivateAccountModal.jsx
@@ -1,6 +1,7 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import btn from "/images/delete.png";
+import btn from "../../../public/images/delete.png";
+
 function DeactivateAccountModal(props) {
   return (
     <>
