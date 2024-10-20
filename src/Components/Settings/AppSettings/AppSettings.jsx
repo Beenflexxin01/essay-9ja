36544438fc7b437845598810d37de85b0eba0088
Omotayo Adenola <adenolaomotayo@gmail.com>
2024-10-20 18:58:28 +0100

    Media Query

--- src/Components/Settings/AppSettings/AppSettings.jsx
+++ src/Components/Settings/AppSettings/AppSettings.jsx
@@ -1,46 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import Button from "../../../UI/Button";
-// import apiCall from "../../../hooks/apiCall";
-// import { BaseUrl } from "../../../Utils/BaseUrl";
-// import AppSet from "./AppSet";
-
-// function AppSettings() {
-//   const [settingsValue, setSettingsValue] = useState([]);
-//   useEffect(() => {
-//     async function getSettingValue() {
-//       try {
-//         const response = await apiCall(`${BaseUrl}/settings`);
-//         if (response && response.data && Array.isArray(response.data.data)) {
-//           setSettingsValue(response.data.data);
-//         } else {
-//           console.log("An unexpected error occured:", response);
-//         }
-//       } catch (err) {
-//         console.log(err);
-//       }
-//     }
-//     getSettingValue();
-//   }, []);
-
-//   return (
-//     <div className="containr">
-//       <div className="settings">
-//         <div className="tert">
-//           <h3 className="tertiary-header">Settings</h3>
-//         </div>
-//         <Button />
-//       </div>
-
-//       {settingsValue &&
-//         settingsValue.map((settingsValue) => (
-//           <AppSet settingsValue={settingsValue} key={settingsValue._id} />
-//         ))}
-//     </div>
-//   );
-// }
-
-// export default AppSettings;
-
 import React, { useEffect, useState } from "react";
 import Button from "../../../UI/Button";
 import apiCall from "../../../hooks/apiCall";
@@ -75,13 +32,9 @@ function AppSettings() {
         <Button />
       </div>
 
-      {Array.isArray(settingsValue) ? (
-        settingsValue.map((setting) => (
-          <AppSet settingsValue={setting} key={setting._id} />
-        ))
-      ) : (
-        <p>No settings available.</p>
-      )}
+      {settingsValue.map((setting) => (
+        <AppSet settingsValue={setting} key={setting._id} />
+      ))}
     </div>
   );
 }
